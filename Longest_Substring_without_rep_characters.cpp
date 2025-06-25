94125e126a02300cd23b466139dc0146e6adca95
HumbleSwagger <39756792+satwaghole@users.noreply.github.com>
2025-06-25 23:40:19 +0530

    Create Longest_Substring_without_rep_characters.cpp
    Brute force way - Without sliding window approach

---
+++ Longest_Substring_without_rep_characters.cpp
@@ -0,0 +1,73 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+/*
+
+Longest substring without repeating characters -- This is non sliding window approach
+
+Given a string s, find the length of the longest substring without duplicate characters.
+
+ 
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+
+Constraints:
+
+0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces.
+
+*/
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) 
+    {
+        int MaxCount=0,count=1;
+        string s1;
+
+        for(int i=0; i<s.size();i++)
+        {
+            s1+=s[i];
+            int j;
+            for(j=i+1; j<s.size();j++)
+            {
+                if(s1.find(s[j]) == string::npos) 
+                {
+                  s1+=s[j];  
+                  count++;
+                }
+                else
+                    break; 
+            }
+            if(count>MaxCount)
+                MaxCount=count;
+            count=1;
+            s1.erase();
+        }     
+        return MaxCount;
+    }
+};
+
+int main()
+{
+	Solution *s1;	
+	cout<<" answer is = "<<s1->lengthOfLongestSubstring("dvdf");	
+	return 0;	
+}
